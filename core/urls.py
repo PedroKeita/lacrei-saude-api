8a80f15492cbaa3d3698d601e820ae5624889e50
PedroKeita <pedrolucas300805@gmail.com>
2026-02-13 14:43:32 -0300

    build: implemented sentry monitoring

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
 
     path("admin/", admin.site.urls),
+
+
+   
 
     path("api/professionals/", include("professionals.urls")),
     path("api/appointments/", include("appointments.urls")),
